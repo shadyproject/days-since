d431d5f47d5450415a94e995e0ab471e79b11d71
Shady <shady@shadyproject.net>
2013-08-03 17:36:58 -0700

    Added Add Achievement view controller and xib

---
+++ DaysSince/DSAddAchievementViewController.h
@@ -0,0 +1,17 @@
+//
+//  DSAddAchievementViewController.h
+//  DaysSince
+//
+//  Created by Christopher Martin on 8/3/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSAddAchievementViewController : UIViewController <UINavigationControllerDelegate>
+{
+    
+}
+
+
+@end
---
+++ DaysSince/DSAddAchievementViewController.m
@@ -0,0 +1,13 @@
+//
+//  DSAddAchievementViewController.m
+//  DaysSince
+//
+//  Created by Christopher Martin on 8/3/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "DSAddAchievementViewController.h"
+
+@implementation DSAddAchievementViewController
+
+@end
