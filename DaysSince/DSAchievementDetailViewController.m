b589db1f442d0e77d9b9a42453af27a43ae8a575
Shady <shady@shadyproject.net>
2013-08-03 21:58:56 -0700

    Implemented viewDidLoa

--- DaysSince/DSAchievementDetailViewController.m
+++ DaysSince/DSAchievementDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DSAchievementDetailViewController.h"
+#import "DSAchievement.h"
 
 @implementation DSAchievementDetailViewController
 
@@ -20,7 +21,17 @@
 }
 
 -(void)viewWillAppear:(BOOL)animated{
-    //draw the achievement info here
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterLongStyle];
+    _dateLabel.text = [NSString stringWithFormat:@"On %@", [dateFormatter stringFromDate:achievement.firstDate]];
+    
+    _startedStoppedLabel.text = [NSString stringWithFormat:@"I %@", (achievement.startedOrStopped == DSAchievementTypeStarted ? @"started" : @"stopped")];
+    
+    _achievementLabel.text = achievement.achievement;
+    
+    _currentStreakLabel.text = [NSString stringWithFormat:@"Thats %d days!", achievement.currentStreak];
+    
+    _longestStreakLabel.text = [NSString stringWithFormat:@"My longest streak is %d days.", achievement.longestStreak];
 }
 
 #pragma mark -
