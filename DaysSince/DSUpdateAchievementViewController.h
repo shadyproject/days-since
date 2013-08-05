e6493630583f5644a4d8c6d35dfc36671a260929
Shady <shady@shadyproject.net>
2013-08-04 20:12:00 -0700

    Add Update Tracking view

---
+++ DaysSince/DSUpdateAchievementViewController.h
@@ -0,0 +1,12 @@
+//
+//  DSUpdateAchievementViewController.h
+//  DaysSince
+//
+//  Created by Christopher Martin on 8/4/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSUpdateAchievementViewController : UIViewController <UINavigationControllerDelegate>
+@end
---
+++ DaysSince/DSUpdateAchievementViewController.m
@@ -0,0 +1,13 @@
+//
+//  DSUpdateAchievementViewController.m
+//  DaysSince
+//
+//  Created by Christopher Martin on 8/4/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "DSUpdateAchievementViewController.h"
+
+@implementation DSUpdateAchievementViewController
+
+@end
